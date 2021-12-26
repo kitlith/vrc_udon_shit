c6ac1e18ef0400aad9134219c11ab72b3639f5fc
HookedBehemoth <LuisScheurenbrand@gmail.com>
2021-12-26 10:29:15 +0100

    make span hold pointer instead of reference this is to avoid indexing into empty spans on externs that take zero arguments

--- native/src/span.rs
+++ native/src/span.rs
@@ -1,17 +1,17 @@
 use libc::c_void;
 
 /* Note: Blittable */
-pub struct Span<'a, T> {
+pub struct Span<T> {
     pub pinnable: *const c_void,
-    pub byte_offset: &'a T,
+    pub byte_offset: *const T,
     pub length: i32,
 }
 
-impl<'a, T> Span<'a, T> {
-    pub fn new(slice: &'a [T]) -> Span<'a, T> {
+impl<T> Span<T> {
+    pub fn new(slice: &[T]) -> Span<T> {
         Span {
             pinnable: 0 as *const c_void,
-            byte_offset: &slice[0],
+            byte_offset: slice.as_ptr(),
             length: slice.len() as i32,
         }
     }
